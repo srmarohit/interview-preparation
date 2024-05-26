6256b4607c09f13b4c7356993f8d41f4e3e2828b
srmarohit <srmarohit@gmail.com>
2024-05-26 16:16:10 +0530

    feature/lam-hcf: find LCM and HCF of three numbers

--- rohit/js/find-hcf-lcm.js
+++ rohit/js/find-hcf-lcm.js
@@ -9,7 +9,7 @@ function findHCF(a, b) {
   return hc;
 }
 
-console.log(findHCF(45, 60));
+console.log("LCM of 45,60 = " + findHCF(45, 60));
 
 function findLCM(a, b) {
   let lc = (a * b) / findHCF(a, b);
@@ -17,4 +17,21 @@ function findLCM(a, b) {
   return lc;
 }
 
-console.log(findLCM(10, 45));
+console.log("LCM of 10,45 = " + findLCM(10, 45));
+
+// find HCF by using Euclidean's Law
+function getHCF(a, b) {
+  if (b == 0) {
+    return a;
+  }
+  return getHCF(b, a % b);
+}
+
+// get HCF of two numbers
+console.log("HCF of 30,45 = " + getHCF(30, 45));
+
+// get HCF of three numbers
+console.log("HCF of 30,45,60 = " + getHCF(getHCF(30, 45), 60));
+
+// get LCM of three numbers
+console.log("LCM of 30,40,60 = " + findLCM(findLCM(30, 40), 60));
